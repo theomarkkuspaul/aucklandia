9d4a10d9c99a5a5b0f241c6987f63669f9c93c37
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2018-08-27 21:16:12 +1200

    Add base url endpoint for AT API

--- lib/aucklandia/client.rb
+++ lib/aucklandia/client.rb
@@ -1,5 +1,7 @@
 module Aucklandia
   class Client
+    BASE_URL = "https://api.at.govt.nz/v2"
+
     attr_reader :authorization_key
     def initialize(authorization_key)
       @authorization_key = authorization_key
