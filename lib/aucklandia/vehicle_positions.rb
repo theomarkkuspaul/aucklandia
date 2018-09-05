1b8a02367dfd2f203f2597f46763f48c12a632b5
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2018-09-05 19:45:32 +1200

    Fetch all vehicle positions by route id

--- lib/aucklandia/vehicle_positions.rb
+++ lib/aucklandia/vehicle_positions.rb
@@ -11,5 +11,11 @@ module Aucklandia
 
       JSON.parse(response)['response']['entity']
     end
+
+    def get_vehicle_positions_by_route_id(route_id)
+      get_vehicle_positions.select do |vehicle_position|
+        vehicle_position if vehicle_position['vehicle']['trip']['route_id'] == route_id
+      end
+    end
 	end
 end
