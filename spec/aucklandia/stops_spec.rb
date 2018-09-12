1f9b499284cafdedc9716d26c89c4dd97084b3ef
Theo Paul <theo@boost.co.nz>
2018-09-12 17:39:45 +1200

    Update Stops#get_stops_by_trip_id

--- spec/aucklandia/stops_spec.rb
+++ spec/aucklandia/stops_spec.rb
@@ -4,15 +4,27 @@ RSpec.describe Aucklandia::Stops do
   let(:client) { Aucklandia::Client.new(ENV['AUCKLANDIA_SECRET']) }
 
 	describe '#get_stops_by_trip_id' do
-		let(:trip_id) { '476136543-20180815114333_v70.9' }
 		context 'with trip id parameter' do
-			it 'responds with a collection of stops', vcr: true do
-				client.get_stops_by_trip_id(trip_id).each do |stop|
-					expect(stop).to have_key 'stop_id'
-					expect(stop).to have_key 'stop_name'
-					expect(stop).to have_key 'stop_lat'
-					expect(stop).to have_key 'stop_lon'
-					expect(stop).to have_key 'stop_code'
+			let(:trip_id) { '476136543-20180815114333_v70.9' }
+			context 'valid trip id' do
+				it 'responds with a collection of stops', vcr: true do
+					client.get_stops_by_trip_id(trip_id).each do |stop|
+						expect(stop).to have_key 'stop_id'
+						expect(stop).to have_key 'stop_name'
+						expect(stop).to have_key 'stop_lat'
+						expect(stop).to have_key 'stop_lon'
+						expect(stop).to have_key 'stop_code'
+					end
+				end
+			end
+
+			context 'invalid trip id' do
+				let(:trip_id) { 'delicious' }
+				it 'responds with an empty collection', vcr: true do
+					response = client.get_stops_by_trip_id(trip_id)
+
+					expect(response).to be_an Array
+					expect(response).to be_empty
 				end
 			end
 		end
