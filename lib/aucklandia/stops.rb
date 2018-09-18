08a8c9204039f6626825842f31e6f86aac70cba6
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2018-09-18 21:08:45 +1200

    Indentation

--- lib/aucklandia/stops.rb
+++ lib/aucklandia/stops.rb
@@ -1,14 +1,14 @@
 module Aucklandia
-	module Stops
-		STOPS_ENDPOINT = '/gtfs/stops'
+  module Stops
+    STOPS_ENDPOINT = '/gtfs/stops'
 
-		def get_stops_by_trip_id(trip_id)
-			url = build_url(BASE_URL, STOPS_ENDPOINT, '/tripId/', trip_id)
+    def get_stops_by_trip_id(trip_id)
+      url = build_url(BASE_URL, STOPS_ENDPOINT, '/tripId/', trip_id)
 
-			response = get(url)
+      response = get(url)
 
-			JSON.parse(response)['response']
-		end
+      JSON.parse(response)['response']
+    end
 
-	end
+  end
 end
